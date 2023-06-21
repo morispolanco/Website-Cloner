c1e9a5222db1e3f0b15aa028b06b8fab78d9b3e7
morispolanco <morispolanco@gmail.com>
2023-06-21 07:38:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import sys
-
 import os
 import requests
 import shutil
 from bs4 import BeautifulSoup
+import streamlit as st
 
 
 base_dir = os.getcwd()
@@ -12,7 +12,7 @@ try:
     site_name = sys.argv[1]
     project_name = sys.argv[2]
 except IndexError:
-    print("Usage:\npython app.py www.example.com folder_name")
+    st.write("Usage:\npython app.py www.example.com folder_name")
     sys.exit(1)
 
 project_path = "../" + project_name
@@ -30,7 +30,7 @@ def save(bs, element, check):
         if href is not None and href not in visited_links:
             if check in href:
                 href = l.get("href")
-                print("Working with : {}".format(href))
+                st.write("Working with : {}".format(href))
                 if "//" in href:
                     path_s = href.split("/")
                     file_name = ""
@@ -68,7 +68,7 @@ def save_assets(html_text):
     for l in links:
         href = l.get("src")
         if href is not None and href not in visited_links:
-            print("Working with : {}".format(href))
+            st.write("Working with : {}".format(href))
             if "//" in href:
                 path_s = href.split("/")
                 file_name = ""
@@ -101,7 +101,7 @@ def crawl(link):
         link = site_name + link
 
     if site_name in link and link not in visited_links:
-        print("Working with : {}".format(link))
+        st.write("Working with : {}".format(link))
 
         path_s = link.split("/")
         file_name = ""
@@ -114,37 +114,9 @@ def crawl(link):
         try:
             r = requests.get(link)
         except requests.exceptions.ConnectionError:
-            print("Connection Error")
+            st.write("Connection Error")
             sys.exit(1)
 
         if r.status_code != 200:
-            print("Invalid Response")
-            sys.exit(1)
-        print(project_path + file_name + "index.html")
-        os.makedirs(os.path.dirname(project_path + file_name.split("?")[0]), exist_ok=True)
-        with open(project_path + file_name.split("?")[0] + "index.html", "wb") as f:
-            text = r.text.replace(site_name, project_name)
-            f.write(text.encode('utf-8'))
-            f.close()
-
-        visited_links.append(link)
-
-        save_assets(r.text)
-
-        soup = BeautifulSoup(r.text, "html.parser")
-
-        for link in soup.find_all('a'):
-            try:
-                crawl(link.get("href"))
-            except:
-                error_links.append(link.get("href"))
-
-
-crawl(site_name + "/")
-print("Link crawled\n")
-for link in visited_links:
-    print("---- {}\n".format(link))
-
-print("\n\n\nLink error\n")
-for link in error_links:
-    print("---- {}\n".format(link))
+            st.write("Invalid Response")
+           
